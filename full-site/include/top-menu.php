5647b8a97247e7641224788d74f1acb81439abf3
Begas Anna <anneta.begas@gmail.com>
2018-09-18 15:55:58 +0300

    edit top-menu

--- full-site/include/top-menu.php
+++ full-site/include/top-menu.php
@@ -71,7 +71,7 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
 <? /* <div class="saga_logo <?if(count(explode("/", $_SERVER['REQUEST_URI']))>3){echo 'saga_logo_off';}?>"><a href="http://saga-development.com.ua/" class="saga-logo" target="_blank">
 <img src="/img/Saga_white.svg">
 </a></div> */ ?>
-             <? /* 
+             <? /*
              Вот это меню выбора языка от 14.01.18 оно не работало корректно на странице новостей -- Андрей
               <div class="lang">
                <?
@@ -114,9 +114,7 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
               </div>
 				<?php
 				function ringo_phone($ringo_url){
-					
-					
-					
+
 					//$ringo_url = $_SERVER['REQUEST_URI'];
 					$com_url[0] = "kommercheskaya";
 					$com_url[1] = "commercial-lease";
@@ -126,17 +124,17 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
 					$com_url[5] = "restaurant";
 					$com_url[6] = "medicine";
                     $result = 0;
-					
+
 				foreach ($com_url as $value) {
 				if((stripos($ringo_url, $value) !== FALSE)){
 				 $result = 1;
-					
+
 				}
                 }
-				
-				
-                return $result;	 
-				} 
+
+
+                return $result;
+				}
 				?>
               <div class="call-phone">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.250072 49.999996" width="14" height="38" fill="#ffffff" preserveAspectRatio="none"><path d="M15.293 49.977c-.827.004-2.038.007-2.506.014l-.418.004-.695.006C8.305 50.02-.125 45.526 0 24.36.114 4.267 7.857.36 10.91.03 12.59 0 14.64 0 15.507 0l.003.003a3.725 3.725 0 0 1 3.74 3.76l-.026 6.72c-.008 2.09-1.7 3.788-3.776 3.798l.003.004c-.977.006-3.34.013-3.34.013l-1.252.007c-2.032.03-2.33 2.45-2.58 4.955-.042.1.04 10.002.063 10.05.47 4.372.505 6.556 2.446 6.528l4.166-.02c2.076-.01 4.16 1.55 4.15 3.637l-.033 6.72c-.01 2.088-1.703 3.79-3.777 3.802zM13.38 12.575c.227-.003.485 0 .885-.003 2.415-.01 3.282-.63 3.29-2.588l.025-5c.007-2.164-.304-3.33-3.317-3.312-.288.003-.563 0-.833.003l-.05 10.9zM8.31 36.885c-1.393-1.042-1.9-6.073-1.936-9.522-.01-2.61-.38-10.91 1.673-13.77.597-.852 1.886-1.004 2.434-1 .522-.008.904-.01 1.23-.01l.045-10.83c-1.127.12-2.2.422-3.393 1.225-3.433 2.233-6.93 9.79-6.703 22.083 0 0 .03 9.354 2.315 15.172 1.25 2.826 2.764 6.85 7.582 7.878l.045-10.557c-1.45.027-2.814-.052-3.295-.668zm9.097 7.252l.012-3.334c.01-2.844-1.436-3.323-3.954-3.31l-.193.01-.048 10.825c.29.018.578.038.883.035 2.92-.016 3.285-1.255 3.3-4.226z"/></svg>
@@ -174,6 +172,7 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
                   <a class="menu__link a_hover" href="<?=$l?>documents/"><?=$mes['jur-mes-menu']?></a>
                   <a class="menu__link a_hover" href="<?=$l.$menu_t[14]?>"><?=$mes['menu-mes20']?></a>
                   <a class="menu__link a_hover" href="<?=$l?>panorama/"><?=$mes['menu-mes34']?></a>
+				  <a class="menu__link" href="<?=$l?>atmosfera/">SFERA Living System</a>
                 </div>
 
                 <div class="left_top_item">
@@ -189,7 +188,7 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
                       <li><a class="menu__link" href="<?=$l.$menu_t[6]?>"><?=$menu['text'][6]?></a></li>
 					  <li><a class="menu__link" href="<?=$l?>animal-space/"><?=$mes['animal-space-h1']?></a></li>
           					  <li><a class="menu__link" href="<?=$l?>transport-infrastructure/"><?=$mes['menu-mes33']?></a></li>
-          					  <li><a class="menu__link" href="<?=$l?>atmosfera/">SFERA Living System</a></li>
+          					  <li><a class="menu__link" href="<?=$l?>parking/"><?=$mes['parking-heading']?></a></li>
                     </ul>
                 </div>
 
@@ -203,8 +202,7 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
 	                   <?endforeach;?>
 	                 </ul>
 	                 <a class="menu__link" href="<?=$l?>filter/"><?=$mes['menu-mes23']?></a>
-                   <a class="menu__link" href="<?=$l?>rassrochka/"><?=$mes['ras-mes-menu']?></a>
-                   <a class="menu__link" href="<?=$l?>parking/"><?=$mes['menu-mes35']?></a>
+	                 <a class="menu__link" href="<?=$l?>rassrochka/"><?=$mes['ras-mes-menu']?></a>
                  </div>
 	               <div class="left_top_item">
   	                <span class='bigger__nolink realty-block__heading'><?=$mes['menu-mes26']?></span>
@@ -224,6 +222,7 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
                     </a>
                   </div>
                   <!--END_logo -->
+                <div class="left_top_item"></div>
                 <div class="left_top_item left_top_item_flex_grow">
                   <span class='bigger__nolink'><?=$menu['text'][15]?></span>
                     <ul>
@@ -246,6 +245,9 @@ if(strripos($_POST['urls'][0],'plan') !== false && count($_POST['urls'][0])==5){
                           </a>
                           <a href="https://www.youtube.com/channel/UCyhlsag-oKGAuIT035iBrEw/videos" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" height="36" viewBox="0 0 30.000007 36" width="30"><path d="M25.376 26.333H23.4l.01-1.148c0-.51.418-.927.93-.927h.126c.513 0 .933.417.933.927l-.024 1.148zm-7.41-2.46c-.502 0-.912.337-.912.75v5.58c0 .413.41.75.91.75.504 0 .915-.337.915-.75v-5.58c0-.413-.412-.75-.915-.75zM30 20.754v10.616c0 2.546-2.21 4.63-4.908 4.63H4.908C2.208 36 0 33.916 0 31.37V20.75c0-2.547 2.208-4.632 4.908-4.632h20.184c2.7 0 4.908 2.085 4.908 4.632zM6.255 32.522V21.337h2.502V19.68l-6.67-.01v1.63l2.082.006v11.216h2.086zm7.5-9.52H11.67v5.973c0 .864.05 1.296-.005 1.448-.17.464-.932.955-1.23.05-.05-.158-.006-.636-.007-1.457l-.008-6.013H8.345l.007 5.918c0 .908-.02 1.584.007 1.892.05.543.032 1.176.536 1.537.938.678 2.737-.1 3.187-1.066l-.004 1.233 1.674.002v-9.516zm6.673 6.84l-.005-4.97c0-1.895-1.42-3.03-3.343-1.497l.01-3.696-2.085.003-.01 12.756 1.713-.026.156-.795c2.19 2.008 3.567.632 3.564-1.778zm6.528-.66l-1.564.01c0 .062-.004.134-.004.212v.872a.854.854 0 0 1-.855.848h-.306a.854.854 0 0 1-.855-.848v-2.294h3.58v-1.348c0-.985-.024-1.97-.106-2.533-.255-1.78-2.756-2.064-4.02-1.152-.396.285-.7.666-.875 1.18-.177.51-.265 1.21-.265 2.1v2.964c0 4.927 5.985 4.23 5.27-.008zm-8.023-16.09c.108.26.275.473.502.633.224.158.51.237.853.237.3 0 .568-.08.8-.25.23-.165.426-.415.584-.747l-.04.818h2.326V3.896h-1.83v7.696a.763.763 0 0 1-1.524 0V3.896h-1.91v6.67c0 .85.014 1.416.04 1.703a2.9 2.9 0 0 0 .2.823zm-7.046-5.585c0-.95.08-1.69.236-2.225A2.48 2.48 0 0 1 12.978 4c.41-.324.937-.487 1.576-.487.538 0 1 .106 1.384.313.387.208.685.478.892.81.21.335.354.68.43 1.03.08.357.117.894.117 1.62v2.5c0 .918-.037 1.592-.108 2.02a3.21 3.21 0 0 1-.46 1.202c-.23.37-.53.646-.894.823-.367.18-.787.266-1.262.266-.53 0-.976-.073-1.344-.226a1.845 1.845 0 0 1-.858-.686 3.044 3.044 0 0 1-.436-1.108c-.088-.433-.13-1.083-.13-1.95V7.508zm1.82 3.928c0 .56.418 1.017.925 1.017.508 0 .923-.457.923-1.017V6.172c0-.56-.415-1.016-.923-1.016-.507 0-.924.457-.924 1.016v5.264zm-6.434 2.658h2.193L9.47 6.51 12.064.013h-2.4L8.286 4.84 6.888 0H4.513l2.758 6.514.005 7.58z" /></svg>
+                          </a>
+                          <a href="https://www.instagram.com/rybalsky_kyiv/" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="26"  width="26" enable-background="new 0 0 512 512" fill="#ffffff" viewBox="0 0 512 512"><path d="m352 0h-192c-88.352 0-160 71.648-160 160v192c0 88.352 71.648 160 160 160h192c88.352 0 160-71.648 160-160v-192c0-88.352-71.648-160-160-160zm112 352c0 61.76-50.24 112-112 112h-192c-61.76 0-112-50.24-112-112v-192c0-61.76 50.24-112 112-112h192c61.76 0 112 50.24 112 112z"/><path d="m256 128c-70.688 0-128 57.312-128 128s57.312 128 128 128 128-57.312 128-128-57.312-128-128-128zm0 208c-44.096 0-80-35.904-80-80 0-44.128 35.904-80 80-80s80 35.872 80 80c0 44.096-35.904 80-80 80z"/><circle cx="393.6" cy="118.4" r="17.056"/></svg>
                           </a>
                         </li>
                       </ul>
